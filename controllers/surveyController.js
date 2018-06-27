f7bca7166cae62b1425da030846a32791d152cd6
Daniel Foster <danieljfoster87@gmail.com>
2018-06-27 19:41:55 -0400

    inital commit of basic layout

---
+++ controllers/surveyController.js
@@ -0,0 +1,38 @@
+const Surveys = require("../models/Surveys");
+
+// Defining methods for the booksController
+//
+module.exports = {
+  findAll: function (req, res) {
+    Surveys
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    Surveys
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    Surveys
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    Surveys
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    Surveys
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
